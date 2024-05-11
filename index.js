de7812eee709648e6d4b6aca53568bfffdb0e4b4
yeasinbinali <yeasinbinali@gmail.com>
2024-05-11 15:16:30 +0600

    specific id getapi added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -14,39 +14,48 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    const roomCollection = client.db('RoyellaDB').collection('rooms');
+    try {
+        const roomCollection = client.db('RoyellaDB').collection('rooms');
 
-    app.get('/rooms', async(req, res) => {
-        
-    })
+        app.get('/rooms', async (req, res) => {
+            const rooms = roomCollection.find();
+            const result = await rooms.toArray();
+            res.send(result);
+        })
 
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.get('/rooms/:id', async(req, res) => {
+            const id = req.params.id;
+            const room = new ObjectId(id);
+            const result = await roomCollection.findOne(room);
+            res.send(result);
+        })
+
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
 app.get('/', (req, res) => {
-  res.send('Royella server is running!')
+    res.send('Royella server is running!')
 })
 
 app.listen(port, () => {
-  console.log(`Royella server is running on port ${port}`)
+    console.log(`Royella server is running on port ${port}`)
 })
